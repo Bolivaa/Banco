00ee3b7a85ca0220185a4a9e2eee74290a5a2e15
Bolivaa <lucas45650@gmail.com>
2025-08-01 22:44:18 -0300

    att

--- banco.py
+++ banco.py
@@ -2,13 +2,16 @@ op = -1
 deposito = 0
 limite_de_saque = 3
 saque_realizado = 0 
-
+extrato = []
 
 while op != 0:
     op = int(input("1 - deposito \n2 - Sacar \n3 - Extrato \n0 - Sair \n" ))
 
     if op == 1:
-        valor = float(input("\n digite valor desejado para deposito: \n"))
+     valor = float(input("\n digite valor desejado para deposito: \n"))
+     if valor < 0:
+          print("Valor invalido para deposito !")
+     else:
         print(f"Deposito no valor de {valor:.2f} realizado com sucesso !")
         deposito += valor
     elif op == 2:
@@ -24,12 +27,23 @@ while op != 0:
                 print("\n❌ Valor maior que o permitido ❌ \n")
                 continue
            else:  
-                 print(f"Saque no valor de {saque:.2f} realizado com sucesso !")   
+                 print(f"Saque no valor de {saque:.2f} realizado com sucesso!")   
+                 extrato.append(f"saque: {saque:.2f}")
                  saque_realizado += 1
                  deposito -= saque
     elif op == 3:
-     
-          print(f"Seu saldo é de: R${deposito:.2f} e as movimentações de saque {saque}")
-    
+          if extrato:
+               for item in extrato:
+                    print(item)
+               print(f"Seu saldo é de: R${deposito:.2f} e as movimentações:{extrato}")
+          else:          
+           print(f"Sem Transações realiz")
+          
+
+
+
+    elif op == 0:
+         print("Operação finalizada !!!")
+         
     else:
          print("\n❌ Opção Invalida ❌ \n")
